87c3c0094bb07086cdadf7f7f18a72eed009ebd1
nazi <nazipaeez777@gmail.com>
2024-09-07 22:38:21 +0330

    Create articles migration

---
+++ src/migrations/articles-ddl.sql
@@ -0,0 +1,14 @@
+CREATE TABLE articles (
+    id int(10) unsigned NOT NULL AUTO_INCREMENT,
+    title varchar(255) NOT NULL,
+    content text NOT NULL,
+    slug varchar(50) NOT NULL,
+    author_id int(10) unsigned NOT NULL,
+    created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (id),
+    UNIQUE KEY articles_unique_slug (slug),
+    KEY articles_author_fk (author_id),
+    CONSTRAINT articles_author_fk FOREIGN KEY (author_id) REFERENCES users (id) ON DELETE CASCADE
+) ENGINE = INNODB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
