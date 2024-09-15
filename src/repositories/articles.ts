b5754795387137a5f64f080cf8a59ae010538327
nazi <nazipaeez777@gmail.com>
2024-09-15 22:45:53 +0330

    Refactor getAll articles repo

--- src/repositories/articles.ts
+++ src/repositories/articles.ts
@@ -1,6 +1,8 @@
 import db from "../db";
 import { RowDataPacket } from "mysql2";
 import { IArticle, articleTypes } from "../models/article.model";
+import { calculateRelativeTimeDifference } from "../utils/formatTime";
+import { ITag } from "../models/tag.model";
 
 export const create = async ({
   title,
@@ -35,10 +37,46 @@ export const create = async ({
 
 export const getAll = async () => {
   try {
-    const query = "SELECT * FROM articles ORDER BY id DESC";
+    const query = `SELECT articles.id,articles.title,articles.content,articles.slug,articles.created_at,users.name,users.username,users.avatar
+         FROM articles
+         JOIN users ON
+         articles.author_id = users.id
+         ORDER BY articles.id DESC`;
     const [articles] = await db.execute<IArticle[]>(query);
 
-    return articles;
+
+    const formattedArticles = []
+
+    for(const article of articles){
+
+      const [tags] = await db.execute<ITag[]>(`SELECT tags.* FROM articles_tags
+        JOIN tags ON
+      articles_tags.tag_id = tags.id
+      WHERE articles_tags.article_id = ?`,[article.id]);
+
+      (article as any).created_at = calculateRelativeTimeDifference(article.created_at)
+
+       formattedArticles.push({
+
+        id:article.id,
+        title:article.title,
+        content:article.content,
+        slug:article.slug,
+        cover:article.cover,
+        author:{
+          name:article.name,
+          username:article.username,
+          avatar:article.avatar
+        },
+        tags:tags.map((tag) => tag.title),
+        createdAt:article.created_at
+       })
+      
+
+      
+    }
+
+    return formattedArticles;
   } catch (error) {
     throw error;
   }
@@ -55,11 +93,11 @@ export const remove = async (id: number) => {
   }
 };
 
-export const addTag = async (articleId: number,tagId:number) => {
+export const addTag = async (articleId: number, tagId: number) => {
   try {
     const query = "INSERT INTO articles_tags VALUES(NULL,?,?)";
 
-    await db.execute(query, [articleId,tagId]);
+    await db.execute(query, [articleId, tagId]);
 
     return true;
   } catch (error) {
